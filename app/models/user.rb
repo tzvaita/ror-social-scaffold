40d843eb1e02aa5acf388d3f898d71839dc6d2c8
tzvaita <tzvaita@gmail.com>
2020-05-11 11:31:34 +0200

    Add methods to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,25 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :friendships
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
+
+  def friends
+    result = []
+    friendships.where(friendship_status: true).each do |x|
+      result << x.friend
+    end
+    result
+  end
+
+  def inverse_friends
+    result = [self]
+    inverse_friendships.where(confirmed: true).each do |x|
+      result << x.user
+    end
+    result
+  end
+
+  def friends_and_own_posts
+    Post.where(user: (friends + inverse_friends))
+  end
+
 end
