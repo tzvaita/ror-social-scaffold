128bbdfe56032f72def4bbf4ad79bb613bd7d19d
tzvaita <tzvaita@gmail.com>
2020-05-11 22:07:28 +0200

    Order posts by most recent

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,7 @@ class PostsController < ApplicationController
 
   def index
     @post = Post.new
-    @posts = current_user.friends_and_own_posts
-    #timeline_posts
+    @posts = current_user.friends_and_own_posts.ordered_by_most_recent
   end
 
   def create
@@ -19,10 +18,6 @@ class PostsController < ApplicationController
   end
 
   private
-
-  def timeline_posts
-    @timeline_posts ||= Post.all.ordered_by_most_recent.includes(:user)
-  end
 
   def post_params
     params.require(:post).permit(:content)
