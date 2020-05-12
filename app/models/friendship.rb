de3d434e7d58cf60d9a3d5e70594960f3aafb490
tzvaita <tzvaita@gmail.com>
2020-05-12 23:30:01 +0200

    Add same user_id validation

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,4 +3,5 @@ class Friendship < ApplicationRecord
   belongs_to :friend, class_name: 'User'
 
   validates_uniqueness_of :user_id, :scope => :friend_id
+  validates :friend_id, exclusion: {in: ->(u) { [u.user_id] }}
 end
